b000fd8604ad798a86c6530bcb6ffdb121be5726
Alexander Morales <amoralesmm@yahoo.com>
2021-09-02 00:07:32 -0500

    Learn Async Functions and finalized Function Chapters

---
+++ Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharpAtWorkDuringBreakandLunch
+{
+    class Notes
+    {
+        public static void Notepad()
+        {
+            /*
+             Constructors - Special type of method to create a object. Advantages can initial values when initial.
+            Finalizer - is opposite of constructors
+            Properties - Allows to get and set variables. 3 types Read & write, Read only and Write Only
+
+             */ 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,21 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace LearningCSharpAtWorkDuringBreakandLunch
 {
     class Program 
 
     {
-
+        static async Task CookMeat() //Asynchronous Functions
+        {
+            Debug.WriteLine("Cook Meat");
+        }
+        static async Task CookVegetatables()
+        {
+            Debug.WriteLine("Cook Vegetables");
+        }
+        static async Task CookSoup()
+        {
+            Debug.WriteLine("Cook Soup");
+        }
+        static async Task CookJuviasFood()
+        {
+            Debug.WriteLine("Cook food for Juvia");
+        }
+        static async void MakeDinner()
+        {
+            await CookMeat();
+            await CookVegetatables();
+            await CookSoup();
+            await CookJuviasFood();
+        }
+        static void Printmessage(string message = "Hello World")
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+        }
       
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter first digit?");
-            string firstNum = Console.ReadLine();
-            Console.WriteLine("Enter second digit?");
-            string secondNum = Console.ReadLine();
-            Console.WriteLine
+            MakeDinner();
+  
+                
+
+            //Printmessage();
+            //Printmessage("Hello Texas");
 
         }
 
@@ -28,5 +56,11 @@ namespace LearningCSharpAtWorkDuringBreakandLunch
             string userName = Console.ReadLine();
             System.Diagnostics.Debug.WriteLine(userName);
         }
+
+        //Console.WriteLine("Enter first digit?");
+        //    string firstNum = Console.ReadLine();
+        //Console.WriteLine("Enter second digit?");
+        //    string secondNum = Console.ReadLine();
+        //Console.WriteLine("You two numbers are " + firstNum);
     }
 }
